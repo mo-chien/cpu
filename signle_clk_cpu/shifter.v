7618585996f0e8921a5fa3aa732d6b50f98e55e9
mo-chien <1615412774@qq.com>
2022-07-23 00:20:58 +0800

    移位器功能错误修正 移位类型判断逻辑修改 续写工作总结至单周期CPU移位器

--- signle_clk_cpu/shifter.v
+++ signle_clk_cpu/shifter.v
@@ -26,10 +26,12 @@ module shifter(
     input [`SHIFT_NUM_width] SHIFT_NUM,
     input [`shift_ctr] Shiftctr,
     output [`datawidth-1:0] shift_result);
-    
+    wire [5:0] shift_n;
+    assign shift_n = 6'd32 - SHIFT_NUM;
     wire [`datawidth-1:0] left,right,A_right;
     assign left = Indata<<SHIFT_NUM;
     assign right = Indata>>SHIFT_NUM;
-    assign A_right = (~(32'b1111_1111_1111_1111_1111_1111_1111_1111>>SHIFT_NUM))| right ;
-    assign shift_result = Shiftctr[1]&Indata[31]? A_right:~Shiftctr[0]?left:right;
+    assign A_right =({32{Indata[`datawidth-1]}}<<shift_n)| right ;
+    //11:算术右移；10：逻辑右移；01，00：左移
+    assign shift_result = Shiftctr[1]&Shiftctr[0]? A_right:~Shiftctr[1]?left:right;
 endmodule
